051e4831f90a65ea0d6a56e714b4e692ff97f51d
Arthur Aquino <ataquino@gmail.com>
2017-09-24 13:31:50 -0300

    set GOROOT path

--- lib/environment.sh
+++ lib/environment.sh
@@ -21,6 +21,7 @@ create_default_env() {
   export NODE_MODULES_CACHE=${NODE_MODULES_CACHE:-true}
   export NODE_ENV=${NODE_ENV:-production}
   export NODE_VERBOSE=${NODE_VERBOSE:-false}
+  export GOROOT="$HOME/.heroku/go"
 }
 
 list_node_config() {
