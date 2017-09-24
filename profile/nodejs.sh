2f3196b70b04375f85337e7cd2f91b4a76f65167
Arthur Aquino <ataquino@gmail.com>
2017-09-24 18:47:26 -0300

    test

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -1,5 +1,4 @@
 export PATH="$HOME/.heroku/go/bin:$HOME/go/bin:$HOME/.heroku/node/bin:$HOME/.heroku/yarn/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
-#export GOROOT="$HOME/.heroku/go"
+export GOPATH="$BUILD_DIR/.heroku/go"
 export NODE_HOME="$HOME/.heroku/node"
 export NODE_ENV=${NODE_ENV:-production}
-echo "home $HOME"
